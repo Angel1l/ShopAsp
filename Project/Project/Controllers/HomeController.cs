4dbf5a0b1f9ab926c18a7ed1614649a0ac335279
Dima Drachuk <drachuk9909@gmail.com>
2022-02-23 21:32:25 +0200

    Сделал домашную страничку

---
+++ Project/Project/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Data.Interface;
+using Project.ViewModels;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAllGuns _gunsRep;
+        
+        public HomeController(IAllGuns gunRep)
+        {
+            _gunsRep = gunRep;
+           
+        }
+        public ViewResult Index()
+        {
+            var homeGuns = new HomeViewModel
+            {
+                favGuns = _gunsRep.getFavGuns
+            };
+            return View(homeGuns);
+        }
+    }
+}
---
+++ Project/Project/ViewModels/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Project.Data.Models;
+using System.Collections.Generic;
+
+namespace Project.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Guns> favGuns { get; set; }
+    }
+}
