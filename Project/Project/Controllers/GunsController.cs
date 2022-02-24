e1fdf2d5846053e9be6141a4457b4bc47ead9e5b
Dima Drachuk <drachuk9909@gmail.com>
2022-02-25 00:05:30 +0200

    Начал делать категории товаров

--- Project/Project/Controllers/GunsController.cs
+++ Project/Project/Controllers/GunsController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Project.Data.Interface;
+using Project.Data.Models;
 using Project.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Project.Controllers
 {
@@ -15,13 +19,37 @@ namespace Project.Controllers
             _gunsCategory = iGunsCategory;
         }
      
-        public ViewResult List()
+        public ViewResult List(string category)
         {
-            ViewBag.Title = "Страница с оружием";
-            GunsListViewModel obj = new GunsListViewModel();
-            obj.allGuns = _allGuns.Guns;
-            obj.gunsCategory = "Pistols";
-            return View(obj);
+            string _category = category;
+            IEnumerable<Guns> guns = null;
+            string gunsCategory = "";
+            if(string.IsNullOrEmpty(category))
+            {
+                guns = _allGuns.Guns.OrderBy(guns => guns.id);
+            }
+            else
+            {
+                if(string.Equals("pistols",category, StringComparison.OrdinalIgnoreCase))
+                {
+                    guns = _allGuns.Guns.Where(g => g.Category.categoryName.Equals("Пистолеты")).OrderBy(guns => guns.id);
+                }
+                else if(string.Equals("snipers", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    guns = _allGuns.Guns.Where(g => g.Category.categoryName.Equals("Снайперские винтовки")).OrderBy(guns => guns.id);
+                }
+
+                gunsCategory = category;
+             
+            }
+            var gunsObj = new GunsListViewModel
+            {
+                allGuns = guns,
+                gunsCategory = gunsCategory,
+            };
+
+            ViewBag.Title = "Страница с оружием";           
+            return View(gunsObj);
         }
     }
 }
--- Project/Project/Startup.cs
+++ Project/Project/Startup.cs
@@ -51,8 +51,11 @@ namespace Project
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
-            app.UseMvcWithDefaultRoute();
-            
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}"),
+            });
             using(var scope = app.ApplicationServices.CreateScope())
             {
                 AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
